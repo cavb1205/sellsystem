83ba003187bcad92edbb7c073f7649f4563e6d96
cavb1205 <cavb.1205@hotmail.com>
2023-03-27 14:49:47 -0300

    admin create aporte

--- Aportes/urls.py
+++ Aportes/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.list_aportes, name='list_aportes'),
+    path('t/<str:tienda_id>/', views.list_aportes, name='list_aportes_admin'),
     path('list/<str:date>/', views.list_aportes_fecha, name='list_aportes_fecha'),
     path('<int:pk>/', views.get_aporte, name='detail_aporte'),
     path('create/', views.post_aporte, name='create_aporte'),
+    path('create/t/<str:tienda_id>/',
+         views.post_aporte, name='create_aporte_admin'),
     path('<int:pk>/update/', views.put_aporte, name='update_aporte'),
     path('<int:pk>/delete/', views.delete_aporte, name='delete_aporte'),
 ]
--- Aportes/views.py
+++ Aportes/views.py
@@ -1,4 +1,3 @@
-import collections
 from typing import OrderedDict
 from urllib import request
 from rest_framework import generics
@@ -11,24 +10,25 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 
-
-
 from .models import *
 from .serializers import AporteSerializer, AporteUpdateSerializer, AporteDetailSerializer
 
 
-
 @api_view(['GET'])
-#@permission_classes([IsAuthenticated])
-def list_aportes(request):
+# @permission_classes([IsAuthenticated])
+def list_aportes(request, tienda_id=None):
     '''obtenemos todas los aportes'''
-    
-    user=request.user
-    aportes = Aporte.objects.filter(tienda=user.perfil.tienda)
+
+    user = request.user
+    if tienda_id:
+        aportes = Aporte.objects.filter(tienda=tienda_id)
+    else:
+        aportes = Aporte.objects.filter(tienda=user.perfil.tienda)
     if aportes:
-         aporte_serializer = AporteDetailSerializer(aportes, many=True)
-         return Response(aporte_serializer.data, status=status.HTTP_200_OK)
-    return Response({'message':'No se han creado aportes'}, status=status.HTTP_200_OK)
+        aporte_serializer = AporteDetailSerializer(aportes, many=True)
+        return Response(aporte_serializer.data, status=status.HTTP_200_OK)
+    return Response({'message': 'No se han creado aportes'}, status=status.HTTP_200_OK)
+
 
 @api_view(['GET'])
 def list_aportes_fecha(request, date):
@@ -40,9 +40,7 @@ def list_aportes_fecha(request, date):
     if aportes:
         aporte_serializer = AporteDetailSerializer(aportes, many=True)
         return Response(aporte_serializer.data, status=status.HTTP_200_OK)
-    return Response({'message':'No se encontraron aportes'}, status=status.HTTP_200_OK)
-
-    
+    return Response({'message': 'No se encontraron aportes'}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
@@ -52,7 +50,7 @@ def get_aporte(request, pk):
         aporte_serializer = AporteSerializer(aporte, many=False)
         return Response(aporte_serializer.data)
     else:
-        return Response({'message':'No se encontro el aporte'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'No se encontro el aporte'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['PUT'])
@@ -60,36 +58,45 @@ def put_aporte(request, pk):
     aporte_inicial = Aporte.objects.filter(id=pk).first()
     tienda = Tienda.objects.filter(id=request.user.perfil.tienda.id).first()
     if aporte_inicial:
-        aporte_serializer = AporteUpdateSerializer(aporte_inicial, data=request.data)
-        if aporte_serializer.is_valid():            
+        aporte_serializer = AporteUpdateSerializer(
+            aporte_inicial, data=request.data)
+        if aporte_serializer.is_valid():
             new_aporte = aporte_serializer.validated_data['valor']
-            if (aporte_inicial.valor <= new_aporte):                        
-                tienda.caja_inicial = tienda.caja_inicial + (new_aporte - aporte_inicial.valor)            
+            if (aporte_inicial.valor <= new_aporte):
+                tienda.caja_inicial = tienda.caja_inicial + \
+                    (new_aporte - aporte_inicial.valor)
             elif (aporte_inicial.valor >= new_aporte):
-                tienda.caja_inicial = tienda.caja_inicial - (aporte_inicial.valor-new_aporte)
+                tienda.caja_inicial = tienda.caja_inicial - \
+                    (aporte_inicial.valor-new_aporte)
             aporte_serializer.save()
             tienda.save()
-            return Response(aporte_serializer.data,status=status.HTTP_200_OK)
+            return Response(aporte_serializer.data, status=status.HTTP_200_OK)
         return Response(aporte_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response({'message':'No se encontró el aporte'}, status=status.HTTP_400_BAD_REQUEST)
-        
+    return Response({'message': 'No se encontró el aporte'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
-def post_aporte(request):
+def post_aporte(request, tienda_id=None):
     '''creamos un aporte'''
+    print('ingresa create aporte')
     if request.method == 'POST':
-        tienda = Tienda.objects.filter(id=request.user.perfil.tienda.id).first()
+        if tienda_id:
+            tienda = Tienda.objects.filter(id=tienda_id).first()
+        else:
+            tienda = Tienda.objects.filter(
+                id=request.user.perfil.tienda.id).first()
         new_data = request.data
-        new_data['tienda']=tienda.id
-        
-        aporte_serializer = AporteSerializer(data = new_data)
+        new_data['tienda'] = tienda.id
+
+        aporte_serializer = AporteSerializer(data=new_data)
         if aporte_serializer.is_valid():
-            aporte_serializer.save()        
-            tienda.caja_inicial = tienda.caja_inicial + aporte_serializer.validated_data['valor']
+            aporte_serializer.save()
+            tienda.caja_inicial = tienda.caja_inicial + \
+                aporte_serializer.validated_data['valor']
             tienda.save()
             return Response(aporte_serializer.data, status=status.HTTP_200_OK)
-        return Response({'message':'Por favor completar los campos del formulario.'}, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response({'message': 'Por favor completar los campos del formulario.'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['DELETE'])
 def delete_aporte(request, pk):
@@ -99,5 +106,5 @@ def delete_aporte(request, pk):
         aporte.delete()
         tienda.caja_inicial = tienda.caja_inicial - aporte.valor
         tienda.save()
-        return Response({'message':'Aporte eliminado correctamente'},status=status.HTTP_200_OK)
-    return Response({'message':'No se encontró el aporte'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'Aporte eliminado correctamente'}, status=status.HTTP_200_OK)
+    return Response({'message': 'No se encontró el aporte'}, status=status.HTTP_400_BAD_REQUEST)
