ed22158fa460985f7d8615cc9857056b6764867b
cavb1205 <cavb.1205@hotmail.com>
2023-03-31 16:59:05 -0300

    change modal in utilidades for route

--- Utilidades/urls.py
+++ Utilidades/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
     
     ### UTILIDADES ####
     path('', views.list_utilidades, name='list_utilidades'),
+    path('t/<str:tienda_id>/', views.list_utilidades, name='list_utilidades_admin'),
     path('list/<str:date>/', views.list_utilidades_x_fecha),
     path('<int:pk>/', views.get_utilidad, name='detail_utilidad'),
     path('create/', views.post_utilidad, name='create_utilidad'),
+    path('create/t/<str:tienda_id>/', views.post_utilidad, name='create_utilidad_admin'),
     path('<int:pk>/update/', views.put_utilidad, name='update_utilidad'),
+    path('<int:pk>/update/t/<str:tienda_id>/', views.put_utilidad, name='update_utilidad_admin'),
     path('<int:pk>/delete/', views.delete_utilidad, name='delete_utilidad'),
+    path('<int:pk>/delete/t/<str:tienda_id>/', views.delete_utilidad, name='delete_utilidad_admin'),
 ]
--- Utilidades/views.py
+++ Utilidades/views.py
@@ -4,30 +4,37 @@ from rest_framework import status
 
 from Utilidades.models import Utilidad
 from Tiendas.models import Tienda
-from Utilidades.serializers import UtilidadSerializer,UtilidadDetailSerializer, UtilidadUpdateSerializer
-
+from Utilidades.serializers import UtilidadSerializer, UtilidadDetailSerializer, UtilidadUpdateSerializer
 
 
 @api_view(['GET'])
-def list_utilidades(request):
+def list_utilidades(request, tienda_id=None):
     '''obtenemos todas las utilidads'''
     user = request.user
-    utilidades = Utilidad.objects.filter(tienda=user.perfil.tienda)
+    if tienda_id:
+        utilidades = Utilidad.objects.filter(tienda=tienda_id)
+    else:
+        utilidades = Utilidad.objects.filter(tienda=user.perfil.tienda)
     if utilidades:
         utilidad_serializer = UtilidadDetailSerializer(utilidades, many=True)
         return Response(utilidad_serializer.data, status=status.HTTP_200_OK)
-    return Response({'message':'No se han creado utilidades'}, status=status.HTTP_200_OK)
-    
-    
+    return Response({'message': 'No se han creado utilidades'}, status=status.HTTP_200_OK)
+
+
 @api_view(['GET'])
-def list_utilidades_x_fecha(request, date):
+def list_utilidades_x_fecha(request, date, tienda_id=None):
     '''obtenemos todas las utilidades por fecha'''
     user = request.user
-    utilidades = Utilidad.objects.filter(tienda=user.perfil.tienda).filter(fecha=date).order_by('-id')
+    if tienda_id:
+        utilidades = Utilidad.objects.filter(
+            tienda=tienda_id).filter(fecha=date).order_by('-id')
+    else:
+        utilidades = Utilidad.objects.filter(
+            tienda=user.perfil.tienda).filter(fecha=date).order_by('-id')
     if utilidades:
         utilidad_serializer = UtilidadDetailSerializer(utilidades, many=True)
         return Response(utilidad_serializer.data, status=status.HTTP_200_OK)
-    return Response({'message':'No se encontraron utilidades'}, status=status.HTTP_200_OK)
+    return Response({'message': 'No se encontraron utilidades'}, status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
@@ -37,52 +44,69 @@ def get_utilidad(request, pk):
         utilidad_serializer = UtilidadSerializer(utilidad, many=False)
         return Response(utilidad_serializer.data, status=status.HTTP_200_OK)
     else:
-        return Response({'message':'No se encontro la utilidad'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'No se encontro la utilidad'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['PUT'])
-def put_utilidad(request, pk):
-    tienda = Tienda.objects.filter(id=request.user.perfil.tienda.id).first()
+def put_utilidad(request, pk, tienda_id=None):
+    if tienda_id:
+        tienda = Tienda.objects.filter(id=tienda_id).first()
+    else:
+        tienda = Tienda.objects.filter(id=request.user.perfil.tienda.id).first()
     utilidad = Utilidad.objects.filter(id=pk).first()
     utilidad_valor = utilidad.valor
     if utilidad:
-        utilidad_serializer = UtilidadUpdateSerializer(utilidad, data=request.data)
+        utilidad_serializer = UtilidadUpdateSerializer(
+            utilidad, data=request.data)
         if utilidad_serializer.is_valid():
             if utilidad_valor < utilidad_serializer.validated_data['valor']:
-                tienda.caja_inicial = tienda.caja_inicial - (utilidad_serializer.validated_data['valor']-utilidad_valor)
-            
-            elif utilidad_valor > utilidad_serializer.validated_data['valor']:
-                tienda.caja_inicial = tienda.caja_inicial + (utilidad_valor - utilidad_serializer.validated_data['valor'])
-            utilidad_serializer.save()
-            tienda.save()
-            return Response(utilidad_serializer.data,status=status.HTTP_200_OK)
-        return Response(utilidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response({'message':'No se encontró la utilidad'}, status=status.HTTP_400_BAD_REQUEST)
-        
+                tienda.caja_inicial = tienda.caja_inicial - \
+                    (utilidad_serializer.validated_data['valor']-utilidad_valor)
 
-@api_view(['POST'])
-def post_utilidad(request):
-    '''creamos una utilidad'''
-    if request.method == 'POST':
-        tienda = Tienda.objects.filter(id=request.user.perfil.tienda.id).first()
-        new_data = request.data
-        new_data['tienda']=tienda.id
-        utilidad_serializer = UtilidadSerializer(data = new_data)
-        if utilidad_serializer.is_valid():
+            elif utilidad_valor > utilidad_serializer.validated_data['valor']:
+                tienda.caja_inicial = tienda.caja_inicial + \
+                    (utilidad_valor -
+                     utilidad_serializer.validated_data['valor'])
             utilidad_serializer.save()
-            tienda.caja_inicial = tienda.caja_inicial - utilidad_serializer.validated_data['valor']
             tienda.save()
             return Response(utilidad_serializer.data, status=status.HTTP_200_OK)
         return Response(utilidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+    return Response({'message': 'No se encontró la utilidad'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def post_utilidad(request, tienda_id=None):
+    '''creamos una utilidad'''
+
+    if request.method == 'POST':
+        if tienda_id:
+            tienda = Tienda.objects.filter(
+                id=tienda_id).first()
+        else:
+            tienda = Tienda.objects.filter(
+                id=request.user.perfil.tienda.id).first()
+        new_data = request.data
+        new_data['tienda'] = tienda.id
+        utilidad_serializer = UtilidadSerializer(data=new_data)
+        if utilidad_serializer.is_valid():
+            utilidad_serializer.save()
+            tienda.caja_inicial = tienda.caja_inicial - \
+                utilidad_serializer.validated_data['valor']
+            tienda.save()
+            return Response(utilidad_serializer.data, status=status.HTTP_200_OK)
+        return Response(utilidad_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['DELETE'])
-def delete_utilidad(request, pk):
-    tienda = Tienda.objects.filter(id=request.user.perfil.tienda.id).first()
+def delete_utilidad(request, pk, tienda_id=None):
+    if tienda_id:
+        tienda = Tienda.objects.filter(id=tienda_id).first()
+    else:
+        tienda = Tienda.objects.filter(id=request.user.perfil.tienda.id).first()
     utilidad = Utilidad.objects.filter(id=pk).first()
     if utilidad:
         tienda.caja_inicial = tienda.caja_inicial + utilidad.valor
         utilidad.delete()
         tienda.save()
-        return Response({'message':'Utilidad eliminada correctamente'},status=status.HTTP_200_OK)
-    return Response({'message':'No se encontró la utilidad'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'Utilidad eliminada correctamente'}, status=status.HTTP_200_OK)
+    return Response({'message': 'No se encontró la utilidad'}, status=status.HTTP_400_BAD_REQUEST)
